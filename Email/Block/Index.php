39422dc0d3b8eca63cf9b4c7499636d6cf7feb8a
Aanchal <168912992+Parikhanchal@users.noreply.github.com>
2024-07-22 10:54:56 +0530

    Add files via upload

---
+++ Email/Block/Index.php
@@ -0,0 +1,9 @@
+<?php
+namespace Acp\Email\Block;
+
+use Magento\Framework\View\Element\Template;
+
+class Index extends Template implements \Magento\Framework\View\Element\BlockInterface
+{
+    // Your block code goes here
+}
---
+++ Email/Controller/Mail/Index.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Acp\Email\Controller\Mail;
+
+class Index extends \Magento\Framework\App\Action\Action
+{
+    protected $transportBuilder;
+    protected $storeManager;
+    protected $inlineTranslation;
+    protected $_pageFactory;
+
+    public function __construct(
+        \Magento\Framework\App\Action\Context $context,
+        \Magento\Framework\Mail\Template\TransportBuilder $transportBuilder,
+        \Magento\Store\Model\StoreManagerInterface $storeManager,
+        \Magento\Framework\Translate\Inline\StateInterface $inlineTranslation,
+        \Magento\Framework\View\Result\PageFactory $pageFactory
+    )
+    {
+        $this->transportBuilder = $transportBuilder;
+        $this->storeManager = $storeManager;
+        $this->inlineTranslation = $inlineTranslation;
+        $this->_pageFactory = $pageFactory;
+ 
+        parent::__construct($context);
+    }
+ 
+    public function execute()
+    {
+        $email = $this->getRequest()->getParam('email1');
+        $templateOptions = ['area' => \Magento\Framework\App\Area::AREA_FRONTEND, 'store' => $this->storeManager->getStore()->getId()];
+        $templateVars = [
+            'store' => $this->storeManager->getStore(),
+            'customer_name' => 'aanchal',
+            'message' => 'test email template!!.'
+        ];
+        $from = ['email' => 'test@g.com', 'name' => 'enni'];
+        $this->inlineTranslation->suspend();
+        $transport = $this->transportBuilder->setTemplateIdentifier('Test_Email_template')
+            ->setTemplateOptions($templateOptions)
+            ->setTemplateVars($templateVars)
+            ->setFrom($from)
+            ->addTo($email)
+            ->getTransport();
+        $transport->sendMessage();
+        $this->inlineTranslation->resume();
+        
+        return $this->_pageFactory->create();
+    }
+}
+ 
